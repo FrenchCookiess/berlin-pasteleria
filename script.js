e57a6e25346a18d502ecbc9e37d8b77385695386
josuesimancas123-hash <josuesimancas123@gmail.com>
2025-09-02 00:26:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Mostrar u ocultar el menú
+document.getElementById("menuBtn").addEventListener("click", () => {
+  const menu = document.getElementById("menuContent");
+  menu.classList.toggle("hidden");
+});
